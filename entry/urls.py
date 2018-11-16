0bf2f0bc5aa294ff6f760073c8333241251217d3
Carlos Farinha <ca.farinhacouce.contractor@mnemo.com>
2018-11-16 10:36:41 +0100

    Cambios individuales

--- entry/urls.py
+++ entry/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accounts_resume, name='resume'),
+    path('<int:pky>/<int:pkm>', views.accounts_resume, name='resume'),
     path('outcomes/<int:pky>/<int:pkm>', views.outcomes_resume, name='outcome'),
     path('incomes', views.incomes_resume, name='income'),
     path('transfers', views.transfers_resume, name='transfer'),
--- entry/views.py
+++ entry/views.py
@@ -7,39 +7,38 @@ from .methods import *
 import datetime
 # Create your views here.
 
-def accounts_resume(request):
+def accounts_resume(request,pky=datetime.datetime.now().year,pkm=datetime.datetime.now().month):
     accounts = Account.objects.all()
     today = datetime.datetime.now()
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     tables = []
-    for i in range(1,12) :
-        starts = []
-        values = [[],[],[],[],[],[]]
-        outcomes = []
-        transfers = []
-        start_date = datetime.date(today.year,i,1)
-        r = calendar.monthrange(today.year,i)
-        end_date = datetime.date(today.year,i,r[1])
-        for account in accounts :
-            values[0].append(round(balance_date(start_date,account),2))
-            values[1].append(round(month_income(start_date, end_date, account),2))
-            values[2].append(round(month_outcome(start_date, end_date, account),2))
-            values[3].append(round(month_transfer(start_date, end_date, account),2))
-        values[4]= [round(float(a) -float(b) + float(c),2) for a,b,c in zip(values[1], values[2], values[3])]
-        values[5]= [round(float(a) +float(b),2) for a,b in zip(values[4], values[0])]
-        total = sum(values[5])
-        incomes = sum(values[1])
-        outcomes = sum(values[2])
-        thismonth = today.month
-        inc = incomes/(incomes+outcomes) if incomes+outcomes >0 else 0
-        ouc = outcomes/(incomes+outcomes) if incomes+outcomes >0 else 0
-        values = [a+b for a,b in zip([['Start'],['Income'],['Outcome'],['Transfer'],['SubTotal'],['Total']],values)]
+    starts = []
+    values = [[],[],[],[],[],[]]
+    outcomes = []
+    transfers = []
+    start_date = datetime.date(pky,pkm,1)
+    r = calendar.monthrange(pky,pkm)
+    end_date = datetime.date(pky,pkm,r[1])
+    for account in accounts :
+        values[0].append(round(balance_date(start_date,account),2))
+        values[1].append(round(month_income(start_date, end_date, account),2))
+        values[2].append(round(month_outcome(start_date, end_date, account),2))
+        values[3].append(round(month_transfer(start_date, end_date, account),2))
+    values[4]= [round(float(a) -float(b) + float(c),2) for a,b,c in zip(values[1], values[2], values[3])]
+    values[5]= [round(float(a) +float(b),2) for a,b in zip(values[4], values[0])]
+    total = sum(values[5])
+    incomes = sum(values[1])
+    outcomes = sum(values[2])
+    thismonth = start_date.month
+    inc = incomes/(incomes+outcomes) if incomes+outcomes >0 else 0
+    ouc = outcomes/(incomes+outcomes) if incomes+outcomes >0 else 0
+    values = [a+b for a,b in zip([['Start'],['Income'],['Outcome'],['Transfer'],['SubTotal'],['Total']],values)]
 
-        tables.append([start_date,end_date,values,round(total,2),inc*100,ouc*100])
+    tables.append([start_date,end_date,values,round(total,2),inc*100,ouc*100])
         
-    return render(request, 'resume.html', {'today':today, 'thismonth':thismonth, 'accounts': accounts, 'tables':tables,'months':months,})
+    return render(request, 'resume.html', {'color': 'blue', 'today':today, 'thismonth':thismonth, 'accounts': accounts, 'tables':tables,'months':months,})
 
-def outcomes_resume(request,pky,pkm):
+def outcomes_resume(request,pky=datetime.datetime.now().year,pkm=datetime.datetime.now().month):
     today = datetime.date(pky,pkm,1)
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     tables = []
@@ -55,12 +54,12 @@ def outcomes_resume(request,pky,pkm):
     line = timeline_graph(start_date,r[1],incomes)
     thismonth = today.month
     tables.append([start_date,end_date,values,round(total,2),circle,line]) 
-    return render(request, 'outcomes.html', {'today':today, 'thismonth':thismonth, 'tables':tables,'months':months,'categories':categories,'days':days})
+    return render(request, 'outcomes.html', {'color': 'red','today':today, 'thismonth':thismonth, 'tables':tables,'months':months,'categories':categories,'days':days})
 
 def outcome_add(request):
     categories = Category.objects.all()
     accounts = Account.objects.all()
-    return render(request, 'add-outcome.html', {'categories':categories,'accounts':accounts})
+    return render(request, 'add-outcome.html', {'color': 'red','categories':categories,'accounts':accounts})
 
 def incomes_resume(request):
     today = datetime.datetime.now()
@@ -69,7 +68,7 @@ def incomes_resume(request):
     values = Income.objects.filter(date__range=[start_date, end_date])
     total = values.aggregate(total=Coalesce(Sum('amount'), 0))['total']
     tables = [start_date,end_date,values,round(total,2)] 
-    return render(request, 'incomes.html', {'today':today, 'tables':tables})
+    return render(request, 'incomes.html', {'color': 'teal', 'today':today, 'tables':tables})
 
 def transfers_resume(request):
     today = datetime.datetime.now()
@@ -78,4 +77,4 @@ def transfers_resume(request):
     values = Transfer.objects.filter(date__range=[start_date, end_date])
     total = values.aggregate(total=Coalesce(Sum('amount'), 0))['total']
     tables = [start_date,end_date,values,round(total,2)] 
-    return render(request, 'transfer.html', {'today':today, 'tables':tables})
+    return render(request, 'transfer.html', {'color': 'purple', 'today':today, 'tables':tables})
